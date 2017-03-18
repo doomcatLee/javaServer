a263b60a99e15b8703de3b280cd8d29efb87d974
Alex Lee <doomcat9167@gmail.com>
2017-03-18 11:17:36 -0700

    add basic

---
+++ src/main/java/app.java
@@ -0,0 +1,39 @@
+public void listenSocket(){
+        try{
+        server = new ServerSocket(4321);
+        } catch (IOException e) {
+        System.out.println("Could not listen on port 4321");
+        System.exit(-1);
+        }
+
+        listenSocketSocketserver.acceptSocket
+        try{
+        client = server.accept();
+        } catch (IOException e) {
+        System.out.println("Accept failed: 4321");
+        System.exit(-1);
+        }
+
+        listenSocketBufferedReaderclientPrintWriter
+        try{
+        in = new BufferedReader(new InputStreamReader(
+        client.getInputStream()));
+        out = new PrintWriter(client.getOutputStream(),
+        true);
+        } catch (IOException e) {
+        System.out.println("Read failed");
+        System.exit(-1);
+        }
+        }
+
+        listenSocket
+        while(true){
+        try{
+        line = in.readLine();
+//Send data back to client
+        out.println(line);
+        } catch (IOException e) {
+        System.out.println("Read failed");
+        System.exit(-1);
+        }
+        }
